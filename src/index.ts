3fa7ee61c883af96c2933704085f63925206543f
sterx17 <disasxster@aol.com>
2023-01-28 19:18:29 -0300

    last file

--- src/index.ts
+++ src/index.ts
@@ -29,61 +29,10 @@ app.use("/post", postRouter)
 
 /**************************** TYPES ******************************/
 
-type authenticationData = {
-   id: string
-}
-
-type user = {
-   id: string,
-   name: string,
-   email: string,
-   password: string
-}
-
-enum POST_TYPES {
-   NORMAL = "normal",
-   EVENT = "event"
-}
-
-type post = {
-   id: string,
-   photo: string,
-   description: string,
-   type: POST_TYPES,
-   createdAt: Date,
-   authorId: string
-}
-
 
 /**************************** ENDPOINTS ******************************/
 
-// app.post('/users', )
 
-// app.post('/post', async (req: Request, res: Response) => {
-//    try {
-//       let message = "Success!"
-
-//       const { photo, description, type, authorId } = req.body
-
-//       const postId: string = Date.now().toString()
-
-//       await connection("labook_posts")
-//          .insert({
-//             id:postId,
-//             photo,
-//             description,
-//             type,
-//             author_id: authorId
-//          })
-
-//       res.status(201).send({ message })
-
-//    } catch (error:any) {
-//       let message = error.sqlMessage || error.message
-//       res.statusCode = 400
-//       res.send({ message })
-//    }
-// })
 
 // app.get('/posts/:id', async (req: Request, res: Response) => {
 //    try {
